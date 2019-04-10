9f066d5e1d2d824f716e8bb903e9c5fc8c21b389
cherish <825829416@qq.com>
2019-04-10 22:25:32 +0800

    add shopcart and pay function

--- graduation-web/src/main/webapp/js/help.js
+++ graduation-web/src/main/webapp/js/help.js
@@ -4,3 +4,11 @@ function GetQueryString(name)
     var r = window.location.search.substr(1).match(reg);
     if(r!=null)return  unescape(r[2]); return null;
 }
+
+function getParam(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null)
+        return decodeURI(r[2]);   //对参数进行decodeURI解码
+    return null;
+    }
